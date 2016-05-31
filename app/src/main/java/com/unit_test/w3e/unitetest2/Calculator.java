07b7c7c38ada9978e75a231f0c24b7b513739e0e
faisal_android <faisal_android@w3engineers.com>
2016-05-31 12:03:25 +0600

    thisis test

---
+++ app/src/main/java/com/unit_test/w3e/unitetest2/Calculator.java
@@ -0,0 +1,22 @@
+package com.unit_test.w3e.unitetest2;
+
+/**
+ * Created by w3e on 5/25/16.
+ */
+public class Calculator {
+    public double sum(double a, double b) {
+        return a+b;
+    }
+
+    public double subtract(double a, double b) {
+        return a-b;
+    }
+
+    public double divide(double a, double b) {
+        return 0;
+    }
+
+    public double multiply(double a, double b) {
+        return 0;
+    }
+}
